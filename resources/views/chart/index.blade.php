39c925a035b6e66c28a17b6b162ba2fa6749f1cb
khainorazam <75901996+khainorazam@users.noreply.github.com>
2021-09-16 00:03:43 +0800

    Update index.blade.php for burndown chart UI

--- resources/views/chart/index.blade.php
+++ resources/views/chart/index.blade.php
@@ -26,7 +26,109 @@
 @endsection
 
 @section('content')
+<?php
 
+$actualArray = array(10,9,8,6,8,5,4,3,2, 1.2,0);
+
+$idealArray = range(0, 10, 1);
+$idealXArray = array();
+foreach ($idealArray as $value){
+    $value = trim($value);
+    $idealXArray[] = 'Day '.$value;
+}
+?>  
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- jQuery -->
+<script type="text/javascript" charset="utf8" src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
+<script src="https://code.highcharts.com/highcharts.js"></script>
+
+<title> Burndown charts</title>
+
+<div><h3>Burndown Chart</h3></div>
+
+<div id="container-burndown" style="max-width: 1000px; height: 450px;"></div>
+
+<script>
+jQuery(document).ready(function() {
+var doc = $(document);
+$('#container-burndown').highcharts({
+    title: {
+      text: 'Burndown Chart of Sprint',
+      x: -10 //center
+    },
+	scrollbar: {
+                barBackgroundColor: 'gray',
+                barBorderRadius: 7,
+                barBorderWidth: 0,
+                buttonBackgroundColor: 'gray',
+                buttonBorderWidth: 0,
+                buttonBorderRadius: 7,
+                trackBackgroundColor: 'none',
+                trackBorderWidth: 1,
+                trackBorderRadius: 8,
+                trackBorderColor: '#CCC'
+            },
+    colors: ['blue', 'red'],
+    plotOptions: {
+      line: {
+        lineWidth: 3
+      },
+      tooltip: {
+        hideDelay: 200
+      }
+    },
+  
+    xAxis: {
+      categories: <?php echo json_encode($idealXArray);?>
+    },
+    yAxis: {
+      title: {
+        text: 'Remaining Work (days)'
+		
+      },
+			 type: 'linear',
+			 max:10,
+			 min:0,
+			 tickInterval :1
+	 
+    },
+	
+    tooltip: {
+      valueSuffix: ' day',
+      crosshairs: true,
+      shared: true
+    },
+    legend: {
+     layout: 'horizontal',
+      align: 'center',
+      verticalAlign: 'bottom',
+      borderWidth: 0
+    },
+    series: [{
+      name: 'Ideal Burn',
+      color: 'rgba(255,0,0,0.25)',
+      lineWidth: 2,
+	  
+      data: <?php echo json_encode(array_reverse($idealArray));?>
+    }, {
+      name: 'Actual Burn',
+      color: 'rgba(0,120,200,0.75)',
+      marker: {
+        radius: 6
+      },
+      data: <?php echo json_encode($actualArray);?>
+    }]
+  });
+    });
+</script>
+</html>
+
+<!--
 <html>
 <head>
 <br>
@@ -101,4 +203,8 @@
 </body>
 </html>
 
+
+
+
+--->
 @endsection
