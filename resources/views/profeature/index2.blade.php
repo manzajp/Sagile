dfda8642fe12b9167b0dce8c748bf3ca9ca2d15f
khainorazam <75901996+khainorazam@users.noreply.github.com>
2021-09-27 23:01:32 +0800

    Update burndown chart routing

--- resources/views/profeature/index2.blade.php
+++ resources/views/profeature/index2.blade.php
@@ -1,109 +1,128 @@
-@extends('layouts.app2')
-<style>
-        table {
-          font-family: arial, sans-serif;
-          border-collapse: collapse;
-          width: 100%;
+<?php
+
+namespace App\Http\Controllers;
+use App\Project;
+use App\Sprint;
+use App\User;
+use App\UserStory;
+use App\ProductFeature;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Auth;
+
+class ProductFeatureController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(User $id, Project $project)
+    {
+
+        $project = new Project();
+        if (\Auth::check())
+        {
+            $id = \Auth::user()->getId();
+            
         }
+        if($id)
+        {
+            $pro = \App\Project::where('user_id', '=', $id)->get();     
+            return view('profeature.index',['projects'=>$project->all(), 'pros'=>$pro->all()]);
+
+            // $project =\App\Project::where('proj_name', '=', "$proj_name")->get();
+            // return view('profeature.index',['projects'=>$project]);
+        }
+        return view('project.index',['projects'=>$project->all(), 'pros'=>$pro->all()]);
+            
+    }
+
+    public function index2($proj_name)
+    {
+        $project = new Project();
+        $sprint = Sprint::where('proj_name', '=', "$proj_name")->get();
+        return view('profeature.index2',['sprints'=>$sprint, 'projects'=>$project->all()]);
+    }
+
+    public function index3($sprint_id)
+    {
+        $project = new Project();
+        $create = new UserStory();
+        $sprint = new Sprint();
+        $usersprint = new userStory();
         
-        td, th {
-          border: 1px solid #dddddd;
-          text-align: left;
-          padding: 8px;
-        }
+        $userstory = \App\UserStory::where('sprint_id', '=', $sprint_id)->get();
+        //dd($userstory);
+        return view('profeature.index3',['create'=>$create, 'sprint'=>$sprint, 'usersprint'=>$usersprint,'userstories'=>$userstory, 'projects'=>$project->all()])->with('sprint_id', $sprint_id);
         
-        tr:nth-child(even) {
-          background-color: #dddddd;
-        }
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('project.create');
+    }
 
-        .button {
-         background-color: #4CAF50; /* Green */
-         border: none;
-         color: white;
-         padding: 15px 32px;
-         text-align: center;
-         text-decoration: none;
-         display: inline-block;
-         font-size: 16px;
-        }
-</style>
+    public function edit2()
+    {
+        $project = new Project;
+        $status = new Status;
+        return view('userstory.edit',['statuses'=>$status->all(),'userstory'=>$userStory, 'projects'=>$project->all()]);
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
 
-@section('dashboard')
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ProductFeature  $productFeature
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductFeature $productFeature)
+    {
+        //
+    }
 
-      @foreach($projects as $project)
-              <li>
-                  <a href="{{ route('projects.edit', [$project]) }}">
-                  {{ $project->proj_name }} 
-                  </a>
-                          
-              </li>
-      @endforeach
-        
-@endsection
+    
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ProductFeature  $productFeature
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductFeature $productFeature)
+    {
+        //
+    }
 
-@section('content')
-<br><br>
-    <a href="{{route('profeature.index')}}" class="button">Project List</a>
-<br><br><br>
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ProductFeature  $productFeature
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductFeature $productFeature)
+    {
+        //
+    }
 
-@csrf
-<table  id="sprint">
-  <tr>
-      <th>ID</th>
-      <th>Sprint Name</th>
-      <th>Description</th>
-      <th>Start Date</th>
-      <th>End Date</th>
-      <th>Task Assign To</th>
-      <th>Edit</th>
-      <th>View</th>
-      <th>Burndown Chart</th>
-  </tr>
-  
-  @foreach($sprints as $sprint)
-    <tr>
-      <th>
-        {{$sprint->sprint_id}}
-      </th>
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ProductFeature  $productFeature
+     * @return \Illuminate\Http\Response
+     */
 
-      <th>
-        {{$sprint->sprint_name}}
-      </th>
-
-      <th>
-        {{$sprint->sprint_desc}}        
-      </th>
-
-      <th>
-        {{$sprint->start_sprint}}
-      </th>
-
-      <th>
-        {{$sprint->end_sprint}}
-      </th>
-
-      <th>
-        {{$sprint->users_name}}
-      </th>
-
-      <th>
-        <a href="{{route('sprints.edit', [$sprint->sprint_id])}}">Edit</a>
-      </th>
-
-      <th>
-          <a href="{{action('ProductFeatureController@index3', $sprint['sprint_id'])}}">View</a>
-      </th>
-      <th>
-        <a href="{{route('chart.index', [$sprint->sprint_name, $sprint->start_sprint, $sprint->end_sprint])}}">Show</a>
-      </th>
-    </tr>
-  
- 
-  @endforeach
-  </table>
-  
-  <br><br><br>
-  
-  <button type="submit"><a href="{{route('sprints.create')}}">Create Sprint</a></button>
- 
-@endsection
+}
